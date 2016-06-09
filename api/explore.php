7066bf41bf61a927f1e2089542f8c2ee069c1adc
Noah Gentile <noah.gentile@gmail.com>
2016-06-09 13:31:33 -0700

    Added explore mysql routine

--- api/explore.php
+++ api/explore.php
@@ -5,16 +5,10 @@ require_once("../include/database.php");
 $response = [];
 
 $db = new Database();
-$sql = "CALL experiences()";
+$sql = "CALL explore()";
 $db->query($sql);
 while($e = $db->get())
 	$response[] = $e;
-
-$db = new Database();
-$sql = "CALL destinations()";
-$db->query($sql);
-while($d = $db->get())
-	$response[] = $d;
 
 shuffle($response);
 echo json_encode($response);
